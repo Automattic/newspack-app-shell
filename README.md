64c1a20edf3b50472940055c08c4aec007911322
Adam Borowski <adam@adamboro.com>
2020-01-28 15:41:07 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Newspack App Shell
---
+++ newspack-app-shell.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Plugin Name: Newspack App Shell
+ * Description: A stripped down version of PWA's App Shell architecture, with the goal of displaying elements that are persistent throughout navigation.
+ * Version: 0.0.0
+ * Author: Automattic
+ * Author URI: https://newspack.blog/
+ * License: GPL2
+ * Text Domain: newspack-app-shell
+ * Domain Path: /languages/
+ *
+ * @package Newspack_App_Shell
+ */
